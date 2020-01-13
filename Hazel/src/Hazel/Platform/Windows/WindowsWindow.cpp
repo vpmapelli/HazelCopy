ca8aaff643522e525185a6865634d218f62c4f7c
Vinicius-Windows <viniciusmapelli@gmail.com>
2020-01-13 20:05:25 -0300

    a few Windows adjustments

--- Hazel/src/Hazel/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Hazel/Platform/Windows/WindowsWindow.cpp
@@ -1,71 +1,166 @@
 #include "WindowsWindow.h"
 
-namespace Hazel {
-	
-	static bool s_GLFWInitialized = false;
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/MouseEvent.h"
 
-	Window* Window::Create(const WindowProps& props)
-	{
-		return new WindowsWindow(props);
-	}
+namespace Hazel
+{
+    static bool s_GLFWInitialized = false;
 
-	WindowsWindow::WindowsWindow(const WindowProps& props)
-	{
-		Init(props);
-	}
+    static void GLFWErrorCallback(int error, const char* description)
+    {
+        HZ_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+    }
 
-	WindowsWindow::~WindowsWindow()
-	{
-		Shutdown();
-	}
+    Window* Window::Create(const WindowProps& props)
+    {
+        return new WindowsWindow(props);
+    }
 
-	void WindowsWindow::Init(const WindowProps& props)
-	{
-		m_Data.Title = props.Title;
-		m_Data.Width = props.Width;
-		m_Data.Height = props.Height;
+    WindowsWindow::WindowsWindow(const WindowProps& props)
+    {
+        Init(props);
+    }
 
-		HZ_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
+    WindowsWindow::~WindowsWindow()
+    {
+        Shutdown();
+    }
 
-		if (!s_GLFWInitialized)
-		{
-			// TODO: glfwTerminate on system shutdown
-			int success = glfwInit();
-			HZ_CORE_ASSERT(success, "Could not intialize GLFW!");
+    void WindowsWindow::Init(const WindowProps& props)
+    {
+        m_Data.Title = props.Title;
+        m_Data.Width = props.Width;
+        m_Data.Height = props.Height;
 
-			s_GLFWInitialized = true;
-		}
+        HZ_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
 
-		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
-		glfwSetWindowUserPointer(m_Window, &m_Data);
-		SetVSync(true);
-	}
+        if (!s_GLFWInitialized)
+        {
+            // TODO: glfwTerminate on system shutdown
+            int sucess = glfwInit();
+            HZ_CORE_ASSERT(sucess, "Could not initalize GLFW!");
 
-	void WindowsWindow::Shutdown()
-	{
-		glfwDestroyWindow(m_Window);
-	}
+            glfwSetErrorCallback(GLFWErrorCallback);
 
-	void WindowsWindow::OnUpdate()
-	{
-		glfwPollEvents();
-		glfwSwapBuffers(m_Window);
-	}
+            s_GLFWInitialized = true;
+        }
 
-	void WindowsWindow::SetVSync(bool enabled)
-	{
-		if (enabled)
-			glfwSwapInterval(1);
-		else
-			glfwSwapInterval(0);
+        m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
+        glfwMakeContextCurrent(m_Window);
+        glfwSetWindowUserPointer(m_Window, &m_Data);
+        SetVSync(true);
 
-		m_Data.VSync = enabled;
-	}
+        glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scandcode, int action, int mods) {
 
-	bool WindowsWindow::IsVSync() const
-	{
-		return m_Data.VSync;
-	}
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
 
-}
+            switch (action)
+            {
+            case GLFW_PRESS:
+            {
+                KeyPressedEvent event(key, 0);
+                data.EventCallback(event);
+            }
+            break;
+            case GLFW_RELEASE:
+            {
+                KeyReleasedEvent event(key);
+                data.EventCallback(event);
+            }
+            break;
+            case GLFW_REPEAT:
+            {
+                KeyPressedEvent event(key, 1);
+                data.EventCallback(event);
+            }
+            break;
+            }
+            });
+
+        glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window) {
+
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+            WindowCloseEvent event;
+            data.EventCallback(event);
+            });
+
+        glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height) {
+
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            data.Height = height;
+            data.Width = width;
+
+            WindowResizeEvent event(width, height);
+            data.EventCallback(event);
+
+            });
+
+        glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xpos, double ypos) {
+
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            MouseMovedEvent event((float)xpos, (float)ypos);
+            data.EventCallback(event);
+            });
+
+        glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods) {
+
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            switch (action)
+            {
+            case GLFW_PRESS:
+            {
+                MouseButtonPressedEvent event(button);
+                data.EventCallback(event);
+            }
+            break;
+            case GLFW_RELEASE:
+            {
+                MouseButtonReleaseEvent event(button);
+                data.EventCallback(event);
+            }
+            break;
+            }
+            });
+
+        glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xoffset, double yoffset) {
+
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            MouseScrolledEvent event((float)xoffset, (float)yoffset);
+
+            data.EventCallback(event);
+            });
+
+    }
+
+    void WindowsWindow::Shutdown()
+    {
+        glfwDestroyWindow(m_Window);
+    }
+
+    void WindowsWindow::OnUpdate()
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(m_Window);
+    }
+
+    void WindowsWindow::SetVSync(bool enabled)
+    {
+        if (enabled)
+            glfwSwapInterval(1);
+        else
+            glfwSwapInterval(0);
+
+        m_Data.VSync = enabled;
+    }
+
+    bool WindowsWindow::IsVSync() const
+    {
+        return m_Data.VSync;
+    }
+} // namespace Hazel
