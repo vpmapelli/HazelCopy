daafe34a1344e35578e8677cf092d4fa7f292654
vpmapelli <viniciusmapelli@gmail.com>
2020-03-04 11:23:11 -0300

    video: Our first triangle

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -38,6 +38,8 @@ namespace Hazel {
         ImGuiLayer* m_ImGuiLayer;
         bool m_Running = true;
         LayerStack m_LayerStack;
+
+        unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
     };
 
     // To be defined in CLIENT
